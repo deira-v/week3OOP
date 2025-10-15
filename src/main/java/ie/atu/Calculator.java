462c645c2872f7b49880b2ac9b8b4b2c84d6b35a
deira <deiravalci@gmail.com>
2025-10-15 12:52:16 +0100

    Added new method called Add

---
+++ src/main/java/ie/atu/Calculator.java
@@ -0,0 +1,8 @@
+package ie.atu;
+
+public class Calculator {
+    public int add(int firstNum, int secondNum){
+        return firstNum + secondNum;
+    }
+    //add, subtract, multiply, divide later
+}
